337511b6d6bc42bc845df195590e3cd2884d501a
Asher Clark <kaladin@prancingpony.local>
2024-07-05 21:44:34 -0700

    updated hero page

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -38,12 +38,12 @@ export type SiteConfig = {
 };
 
 const siteConfig: SiteConfig = {
-    title: 'Austin Clark',
-    subtitle: '...thoughts on life, talent development, and music.',
+    title: 'Austin Clark's Nook',
+    subtitle: '...thoughts on current issues, talent development, and music.',
     description: 'a blog for Austin Clark',
     image: {
         src: '/dante-preview.jpg',
-        alt: 'Dante - Astro.js and Tailwind CSS theme'
+        alt: 'preview photo'
     },
     headerNavLinks: [
         {
@@ -83,12 +83,8 @@ const siteConfig: SiteConfig = {
     ],
     socialLinks: [
         {
-            text: 'Dribbble',
-            href: 'https://dribbble.com/'
-        },
-        {
-            text: 'Instagram',
-            href: 'https://instagram.com/'
+            text: 'LinkedIn',
+            href: 'https://www.linkedin.com/in/austintclark/'
         },
         {
             text: 'X/Twitter',
@@ -96,11 +92,11 @@ const siteConfig: SiteConfig = {
         }
     ],
     hero: {
-        title: 'Hi There & Welcome to My Corner of the Web!',
-        text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. I have a profound appreciation for top-notch software, visual design, and the principles of product-led growth. Feel free to explore some of my coding endeavors on <a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> or follow me on <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",
+        title: 'Hello and Welcome!',
+        text: "I'm **Austin Clark**. This blog is a warm space where I share my insights on the world, talent development, and my love for music. If you somehow arrived to this nook without knowing me personally, please take a look at the <a href='https://austinclark.co/about/'>About</a> page to learn all the things.",
         image: {
             src: '/hero.jpeg',
-            alt: 'A person sitting at a desk in front of a computer'
+            alt: 'Austin Clark with his dog Blue.'
         },
         actions: [
             {
