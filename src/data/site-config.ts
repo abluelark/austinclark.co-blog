ff022f27406a7b41cc97ba703613ba521522f023
Asher Clark <kaladin@prancingpony.local>
2024-07-05 21:18:53 -0700

    google font try 2

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -96,7 +96,6 @@ const siteConfig: SiteConfig = {
         }
     ],
     hero: {
-	href: 'https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap'
         title: 'Hi There & Welcome to My Corner of the Web!',
         text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. I have a profound appreciation for top-notch software, visual design, and the principles of product-led growth. Feel free to explore some of my coding endeavors on <a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> or follow me on <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",
         image: {
